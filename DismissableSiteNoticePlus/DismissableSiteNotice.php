873ca79ea27cf6562cadcb43eac5f77b3e0fc34b
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2011-11-03 12:50:38 +0100

    Add DismissableSiteNoticePlus extension.
    Based on DismissableSiteNotice, revision 49924, by Brion Vibber.
    Localization is based on revision 53429.

---
+++ DismissableSiteNoticePlus/DismissableSiteNotice.php
@@ -0,0 +1,60 @@
+<?php
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'DismissableSiteNoticePlus',
+	'author' => 'Brion Vibber, Patrick Westerhoff',
+	'description' => 'Allows users to close the sitenotice. Based on the [http://www.mediawiki.org/wiki/Extension:DismissableSiteNotice original version] by Brion Vibber.',
+	'url' => 'http://wiki.guildwars.com/wiki/User:Poke',
+);
+
+$wgExtensionMessagesFiles['DismissableSiteNotice'] = dirname(__FILE__) . '/DismissableSiteNotice.i18n.php';
+
+function wfDismissableSiteNoticeBefore( &$notice ) {
+	global $wgMajorSiteNoticeID;
+	
+	wfLoadExtensionMessages( 'DismissableSiteNotice' );
+	$noticeId = intval( $wgMajorSiteNoticeID ) . '.' . intval( wfMsgForContent( 'sitenotice_id' ) );
+	
+	if ( isset( $_COOKIE['dismissSiteNotice'] ) && $_COOKIE['dismissSiteNotice'] == $noticeId ) {
+		return false;
+	}
+	
+	return true;
+}
+
+function wfDismissableSiteNotice( &$notice ) {
+	global $wgMajorSiteNoticeID, $wgUser;
+	
+	if ( !$notice ) {
+		return true;
+	}
+	
+	wfLoadExtensionMessages( 'DismissableSiteNotice' );
+	$noticeId = intval( $wgMajorSiteNoticeID ) . '.' . intval( wfMsgForContent( 'sitenotice_id' ) );
+	$closeText = wfMsg( 'sitenotice_close' );
+	$encNotice = Xml::escapeJsString($notice);
+	$encClose = Xml::escapeJsString( wfMsg( 'sitenotice_close' ) );
+	
+	$notice = <<<EOT
+		<script type="text/javascript">/*<![CDATA[*/
+		function dismissNotice() {
+			var noticeDiv  = document.getElementById( 'mw-dismissable-notice' );
+			var expireDate = new Date();
+			expireDate.setTime( expireDate.getTime() + 30 * 86400 * 1000 ); // 30 days
+			document.cookie = 'dismissSiteNotice=$noticeId; expires=' + expireDate.toGMTString() + '; path=/';
+			noticeDiv.parentNode.removeChild( noticeDiv );
+		}
+		/*]]>*/</script>
+		<table id="mw-dismissable-notice" style="width: 100%; margin-top: 0.5em;" cellpadding="0" cellspacing="0"><tr>
+			<td>$notice</td>
+			<td style="padding-left: 0.5em;">[<a href="javascript:dismissNotice();">$closeText</a>]</td>
+		</tr></table>
+EOT;
+	
+	return true;
+}
+
+$wgHooks['SiteNoticeBefore'][] = 'wfDismissableSiteNoticeBefore';
+$wgHooks['SiteNoticeAfter'][] = 'wfDismissableSiteNotice';
+$wgMajorSiteNoticeID = 1;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MediaWiki extensions
+
+## DismissableSiteNoticePlus
+*DismissableSiteNoticePlus* is a fork of the original [DismissableSiteNotice](http://mediawiki.org/wiki/Extension:DismissableSiteNotice) by Brion Vibber. It features an improved output format that also allows a better styling of the site notice.
