a54aa316ae7b264ba21bc0c56f43f5fb42985ea2
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2011-11-06 21:11:00 +0100

    Update translations.
    - German translation by User:Tera on GuildWiki.de.

--- ListTransclusions/ListTransclusions.i18n.php
+++ ListTransclusions/ListTransclusions.i18n.php
@@ -24,3 +24,25 @@ $messages['en'] = array(
 	'accesskey-t-listtransclusions' => '',
 	'tooltip-t-listtransclusions'   => 'List all transcluded templates and used images of this page',
 );
+
+/**
+ * German (Deutsch)
+ * @author User:Tera on GuildWiki.de
+ */
+$messages['de'] = array(
+	'listtransclusions'             => 'Bilder und Vorlagen',
+	'listtransclusions-desc'        => 'Zeigt alle eingebundenen Vorlagen und Bilder einer Seite an',
+	
+	'listtransclusions-title'       => 'Bilder und Vorlagen auf „$1“',
+	'listtransclusions-backlink'    => '← $1',
+	'listtransclusions-page'        => 'Seite:',
+	'listtransclusions-invalid'     => 'Der angegebene Titel ist ungültig.',
+	'listtransclusions-notexist'    => "Die Seite '''[[:$1]]''' existiert nicht.",
+	'listtransclusions-tpls'        => "'''[[:$1]]''' verwendet folgende ''Vorlagen'':",
+	'listtransclusions-tpls-no'     => "'''[[:$1]]''' verwendet keine Vorlagen.",
+	'listtransclusions-imgs'        => "'''[[:$1]]''' verwendet folgende ''Bilder'':",
+	'listtransclusions-imgs-no'     => "'''[[:$1]]''' verwendet keine Bilder.",
+	
+	'accesskey-t-listtransclusions' => '',
+	'tooltip-t-listtransclusions'   => 'Zeigt alle auf dieser Seite eingebundenen Vorlagen und Bilder an',
+);
